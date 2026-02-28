690e5884f3dd24fecf37d64bb7d9179f603dcbfd
Wade Johnson <wadej93@gmail.com>
2026-02-28 15:44:36 -0800

    Make it MORE minimal!

--- index.ts
+++ index.ts
@@ -1,23 +1,8 @@
-import { CamelCase } from 'type-fest'
+import { CamelCase } from "type-fest";
 
-// type extracted from lodash `transform` function
-// https://github.com/DefinitelyTyped/DefinitelyTyped/blob/3eefc1af52b4742eb3675be6b582d8e1ea92cb16/types/lodash/common/common.d.ts#L236
-type MemoVoidDictionaryIterator<
-  T,
-  K extends string | number | symbol,
-  TResult
-> = (acc: TResult, curr: T, key: K, dict: Record<K, T>) => void
-
-function transform<
-  T extends object,
-  TResult
->(
-  object: T,
-  iteratee: MemoVoidDictionaryIterator<T[keyof T], keyof T, TResult>,
-  accumulator?: TResult
-): TResult {
-  return object as any
+function transform<TResult>(iteratee: any): TResult {
+  return undefined as any;
 }
 
-const camelize = <T extends Record<string, any>>(obj: T): CamelCase<T> =>
-  transform(obj, camelize)
+const camelize = <T extends Record<string, any>>(): CamelCase<T> =>
+  transform(camelize);
